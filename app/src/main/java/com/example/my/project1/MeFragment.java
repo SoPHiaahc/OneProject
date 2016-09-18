8f463ff75a939737e9aca13ed26e8e2ef50d3cff
Yansr <545163450@qq.com>
2016-09-18 15:48:15 +0800

    change1

--- app/src/main/java/com/example/my/project1/MeFragment.java
+++ app/src/main/java/com/example/my/project1/MeFragment.java
@@ -2,6 +2,7 @@ package com.example.my.project1;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,7 +12,10 @@ import android.widget.RelativeLayout;
 
 public class MeFragment extends Fragment {
     @Nullable
-    @Override
+
+    //手机图库请求码
+    public static final int SHOW_MAP_DEPOT = 1;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_me, container, false);
         RelativeLayout myLayout1 = (RelativeLayout) view.findViewById(R.id.re_myinfo);
@@ -19,6 +23,22 @@ public class MeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(),MyUserInfoActivity.class));
+            }
+        });
+
+        RelativeLayout myLayout2 = (RelativeLayout) view.findViewById(R.id.re_xiangce);
+        myLayout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                if (Build.VERSION.SDK_INT < 19) {
+                    intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                } else {
+                    intent = new Intent(
+                            Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                }
+                startActivityForResult(intent, SHOW_MAP_DEPOT);
             }
         });
         return view;
