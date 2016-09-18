605f9abda437995e21fab9e6196c91bef05fe25d
Yansr <545163450@qq.com>
2016-09-18 15:42:25 +0800

    change1

--- app/src/main/java/com/example/my/project1/MeFragment.java
+++ app/src/main/java/com/example/my/project1/MeFragment.java
@@ -1,19 +1,26 @@
 package com.example.my.project1;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class MeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_me, container, false);
-//        TextView txt_content = (TextView) view.findViewById(R.id.tv);
-//        txt_content.setText("第四个Fragment");
+        RelativeLayout myLayout1 = (RelativeLayout) view.findViewById(R.id.re_myinfo);
+        myLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(),MyUserInfoActivity.class));
+            }
+        });
         return view;
     }
     public static MeFragment newInstance(String content) {
