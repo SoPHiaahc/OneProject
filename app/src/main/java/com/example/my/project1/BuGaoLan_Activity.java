745b51673f07876591e7ae40abbb0e2060dc6090
Yansr <545163450@qq.com>
2016-10-07 10:24:51 +0800

    pq

--- app/src/main/java/com/example/my/project1/BuGaoLan_Activity.java
+++ app/src/main/java/com/example/my/project1/BuGaoLan_Activity.java
@@ -5,9 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-/**
- * Created by yan on 2016/9/18.
- */
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 public class BuGaoLan_Activity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +24,25 @@ public class BuGaoLan_Activity extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        init();
+
+    }
+
+    private void init(){
+        WebView myWebView = (WebView) findViewById(R.id.my_webview);
+        WebSettings settings = myWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        //WebView加载web资源
+        myWebView.loadUrl("http://notes.stu.edu.cn/login/Login.jsp?logintype=1");
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading (WebView view,
+                                                    WebResourceRequest request) {
+                return true;
             }
         });
     }
